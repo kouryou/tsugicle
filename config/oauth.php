00e5aef52a9e670950f516127a227461c6de1ca8
kouryou <23131663+kouryou@users.noreply.github.com>
2016-05-04 14:06:55 +0900

    twitter認証を本番用に変えました。

--- config/oauth.php
+++ config/oauth.php
@@ -1,7 +1,29 @@
 <?php
-return [
-    'twitter' => [
-        'consumer_key' => 'KDTgCkBp7Je3693Rz1ABzFzOJ',
-        'consumer_secret' => 'OUVBPpEV3hat9bgkZC1GpEz1yI2Prrh4apcKiYNnZ8Dm84xJfa'
-    ]
-];
+switch ( env('CAKEPHP_ENV') ) {
+    case 'development':
+        return [
+            'twitter' => [
+                'consumer_key' => 'KDTgCkBp7Je3693Rz1ABzFzOJ',
+                'consumer_secret' => 'OUVBPpEV3hat9bgkZC1GpEz1yI2Prrh4apcKiYNnZ8Dm84xJfa'
+            ]
+        ];
+        break;
+
+    case 'staging':
+        return [
+            'twitter' => [
+                'consumer_key' => 'KDTgCkBp7Je3693Rz1ABzFzOJ',
+                'consumer_secret' => 'OUVBPpEV3hat9bgkZC1GpEz1yI2Prrh4apcKiYNnZ8Dm84xJfa'
+            ]
+        ];
+        break;
+
+    case 'production':
+        return [
+            'twitter' => [
+                'consumer_key' => 'F67G2huYukjjNYngTt3Y9WwNO',
+                'consumer_secret' => 'zLTOfApHf97JpE3GOuAxwj133SsY5E3q0CL5DpiWAijZU1NOoZ'
+            ]
+        ];
+        break;
+}
